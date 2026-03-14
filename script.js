0d272b2eda66f8d4c95ca6632360eeeedaede998
jidnyasaarote <jidnyasaarote@gmail.com>
2026-03-14 20:23:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+
+let reveals = document.querySelectorAll(".reveal");
+
+for(let i=0;i<reveals.length;i++){
+
+let windowHeight = window.innerHeight;
+let elementTop = reveals[i].getBoundingClientRect().top;
+let visible = 100;
+
+if(elementTop < windowHeight - visible){
+reveals[i].classList.add("active");
+}
+
+}
+}
